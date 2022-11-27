6276451735ae594cb5a3325aea539ddff3098133
Roel de Cort <git.roel@decort.tech>
2022-11-27 01:50:32 +0100

    added authentication, distro updates and ansible

--- scripts/install-wsl.ps1
+++ scripts/install-wsl.ps1
@@ -1,19 +1,25 @@
 ## First we need to make sure the WSL feature is installed, if not install and reboot afterwards 
 if($null -eq (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux)){
-    ## Enable WSL Feature
+    ## Enable WSL and Virtual Machine Platform features
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
     Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+    
     ## Create temp directory if it does not exist
     $tmp = "C:\temp"
     if($null -eq (Test-Path $tmp)){
+        
         New-Item $tmp
-    }else{}
+    
+    }
+    else{}
+    
     ## Copy the script to the temp directory
     Copy-Item -Path ".\install-wsl.ps1" -Destination $tmp 
 
     ## Create a run once registry key in order for this script to be able to continue after reboot
     Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce
     New-Itemproperty . InstallWSL -propertytype String -value "Powershell $tmp\install-wsl.ps1"
+    
     ## Wait a bit and reboot the machine, continue on 
     Restart-Computer -Wait 1
 }
@@ -23,8 +29,11 @@ if($null -eq (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-
 $wsl = wsl -l | Where-Object {$_.Replace("`0","") -match '22.04'}
 $distro = Ubuntu-20.04
 
-$username = "roelc" # Change this (update to commandline switch)
-
+## Set username and password (if existing install please enter your current user credentials otherwise create a new one)
+Write-Host "Please fill in your username"
+$username = Read-Host # Change this (update to commandline switch)
+Write-Host "Please fill in your password"
+$password = Read-Host -asSecureString
 
 ## If WSL is not installed we install it
 if($null -eq $wsl){
@@ -48,11 +57,15 @@ if($null -eq $wsl){
 wsl --set-default-version 2
 wsl --setdefault Ubuntu-20.04
 
+## Decode the password so it can be used inside the bash shell
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+$plainpassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-$decodedpassword = ConvertFrom-SecureString -SecureString $password -AsPlainText
-wsl -u $username "echo ($password | sudo apt update -y"
-wsl -u $username echo $password | sudo apt upgrade -y
+## Let's make sure all our packages are up-to-date
+wsl -u $username /bin/bash -c "echo $plainpassword | sudo -S apt update -y && sudo -S apt upgrade -y"
 
+## Now install Ansible for the second stage of this deployment
+wsl -u $username /bin/bash -c "echo $plainpassword | sudo -S apt install ansible -y"
 
 
 
