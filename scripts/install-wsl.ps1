72875ba0e3fd69619ab988f951889fb0006156ba
Roel de Cort <roel@decort.tech>
2022-12-03 13:15:41 +0100

    add git ignore

--- scripts/install-wsl.ps1
+++ scripts/install-wsl.ps1
@@ -1,5 +1,6 @@
 ## In order to retrieve and/or install Windows Optional Features this script needs to run with admin privileges
 #Requires -RunAsAdministrator
+
 function Install-WSLFeature{
     ## First we need to make sure the WSL feature is installed, if not install and reboot afterwards 
     if($null -eq (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux)){
