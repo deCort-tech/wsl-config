099040152ffb6f4d16d7030dacfbe5a9cd23c16e
Roel de Cort <roel@decort.tech>
2022-12-03 01:08:50 +0100

    some changes

--- scripts/install-wsl.ps1
+++ scripts/install-wsl.ps1
@@ -1,5 +1,5 @@
 ## In order to retrieve and/or install Windows Optional Features this script needs to run with admin privileges
-#Requires -RunAsAdministrator
+#equires -RunAsAdministrator
 
 ## First we need to make sure the WSL feature is installed, if not install and reboot afterwards 
 if($null -eq (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux)){
@@ -29,8 +29,8 @@ if($null -eq (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-
 
 ## Then we bootstrap the host and install WSL and Ansible
 ## We can check if the correct distro is already installed, if not we run the install command
-$wsl = wsl -l | Where-Object {$_.Replace("`0","") -match '22.04'}
-$distro = "Ubuntu-22.04"
+$wsl = wsl -l | Where-Object {$_.Replace("`0","") -match '20.04'}
+$distro = "Ubuntu-20.04"
 
 ## Set username and password (if existing install please enter your current user credentials otherwise create a new one)
 Write-Host "Please fill in your username"
@@ -38,31 +38,32 @@ $username = Read-Host # Change this (update to commandline switch)
 Write-Host "Please fill in your password"
 $password = Read-Host -asSecureString
 
+## Decode the password so it can be used inside the bash shell
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+$plainpassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
 ## If WSL is not installed we install it
 if($null -eq $wsl){
     ## Install the distro as root so we can create a user
-    wsl --install --d $distro --root
+    wsl --install --d $distro
     ## Set WSL2 as default
     wsl --set-default-version 2
-    ## Set our ubuntu 20.04 image as default
-    wsl --setdefault $distro
+    ## Now we got to wait a few minutes in order for the distro to install before we can continue on
+    Start-Sleep -Seconds 60
     ## Add new user
     wsl -u root -d $distro useradd -m "$username"
     ## Set the password
-    wsl -u root -d $distro sh -c "echo "${username}:${password}" | chpasswd"
     ## Change login shell to bash
     wsl -u root -d $distro chsh -s /bin/bash "$username"
     ## Set the privileges
     wsl -u root -d $distro usermod -aG adm,cdrom,sudo,dip,plugdev "$username"
+    ## Set our ubuntu 20.04 image as default
+    wsl --setdefault $distro
 }
 
 ## Set WSL2 as default and set the Ubuntu-20.04 distro as default (if this is not yet the case already)
 wsl --set-default-version 2
 wsl --setdefault $distro
-
-## Decode the password so it can be used inside the bash shell
-$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
-$plainpassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
 ## Let's make sure all our packages are up-to-date
 wsl -u $username /bin/bash -c "echo $plainpassword | sudo -S apt update -y && sudo -S apt upgrade -y"
